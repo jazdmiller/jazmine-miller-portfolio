34e1042762a48661d2da6af33cc5e99214f04991
Jazmine Miller <jazminedmiller@gmail.com>
2022-08-22 16:26:42 -0400

    home page description and button style

--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -5,7 +5,24 @@ function Home() {
   return (
     <div>
         <div className='wrapper'>
-
+            <div className='left-section'>
+                <div className='content'>
+                    <div className='header-wrapper'>
+                        <h1 className='header swipe-up'>
+                            Jazmine Miller
+                        </h1>
+                    </div>
+                    <div className='description-wrapper'>
+                        <p className='description'>
+                            Adaptive full-stack software engineer with a passion for designing
+                            and producing intuitive front-end experiences.
+                        </p>
+                    </div>
+                    <div className='btn-wrapper'>
+                        <button className='btn'>View Work</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
