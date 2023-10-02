a166b3134b9e0bdd522582168c29474376e42e36
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-02 10:57:28 -0400

    home text styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,14 @@ function Navbar() {
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse py-1 justify-content-between" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
+  <div class="collapse navbar-collapse py-2 justify-content-between" id="navbarNavAltMarkup">
+    <div class="navbar-nav ml-3">
       <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Projects</a>
       <a class="nav-item nav-link" href="#">About</a>
       <a class="nav-item nav-link" href="#">Contact</a>
     </div>
-    <img  className='text-end' src={SunIcon}/>
+    <img  className='darkmode-icon mr-4' src={SunIcon}/>
   </div>
     </nav>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,25 @@ import React from 'react'
 function Home() {
   return (
     <div className='home-page'>
-        
+        <div className='home-container mx-auto'>
+            <div className='container home-wrapper'>
+
+            <div className='row'>
+                <div className='col home-text text-center'>
+                frontend
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col home-text developer-col'>
+                developer
+                </div>
+                <div className='col'>
+                    Jaz
+                </div>
+            </div>
+            </div>
+            
+        </div>
     </div>
   )
 }
