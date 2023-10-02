d9c2efd0b3ed943a447826f4f92068ed8b46e804
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-02 09:45:38 -0400

    create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import SunIcon from '../../public/assets/images/icons8-sun.svg'
+
+function Navbar() {
+  return (
+    <nav className='navbar navbar-expand-lg py-0'>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse py-1 justify-content-between" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Projects</a>
+      <a class="nav-item nav-link" href="#">About</a>
+      <a class="nav-item nav-link" href="#">Contact</a>
+    </div>
+    <img  className='text-end' src={SunIcon}/>
+  </div>
+    </nav>
+  )
+}
+
+export default Navbar
