d20da47ee9f42e87a10c8f06ef46c38259152436
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-05 21:32:51 -0400

    projects page layout, hover animation on projects list

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,31 +6,48 @@ function Projects() {
     <div className='container-fluid text-center projects-container mx-5'>
 
       <div className='row'>
-        <div className='col col-md-6 skills-col d-flex flex-column'>
+        <div className='col-12 col-md-6 skills-col d-flex flex-column'>
           <div className=' row mt-auto justify-content-start mr-0'>
             <span className='skills-label'><img className='mr-3' src={SkillsIcon} />Skills</span>
           </div>
-          <div className='row mr-0 skill-list'>
+          <div className='row mr-0 skill-list '>
             <div className='col'>ReactJs</div>
-            <div className='col'>HTML</div>
-            <div className='col'>CSS</div>
-            <div className='col'>Bootstrap</div>
+            <div  className='col'>HTML</div>
+            <div  className='col'>CSS</div>
+          
           </div>
-          <div className='row mr-0 my-3 skill-list'>
-            <div className='col'>ReactJs</div>
+          <div className='row  text-start mr-0 my-3 skill-list '>
+            <div className='col text-start'>ReactJs</div>
             <div className='col'>HTML</div>
             <div className='col'>CSS</div>
-            <div className='col'>Bootstrap</div>
+            
           </div>
-          <div className='row mr-0 mb-5 skill-list'>
+          <div className='row mr-0 mb-5 skill-list '>
             <div className='col'>ReactJs</div>
             <div className='col'>HTML</div>
             <div className='col'>CSS</div>
-            <div className='col'>Bootstrap</div>
+           
           </div>
         </div>
-        <div className='col col-md-6'>
-          <span><img src={SkillsIcon} />Skills</span>
+
+        <div className='col col-md-6 mr-auto project-col'>
+          <div>
+            <div className='project-title'>Shakn</div>
+            <div className='project-subtext'>Design & Development</div>
+          </div>
+          <div className='pt-4'>
+            <div className='project-title'>Workit Landing Page</div>
+            <div className='project-subtext'>Development</div>
+          </div>
+          <div  className='pt-4'>
+            <div className='project-title'>Shakn</div>
+            <div className='project-subtext'>Design & Development</div>
+          </div>
+          <div  className='pt-4'>
+            <div className='project-title'>Shakn</div>
+            <div className='project-subtext'>Design & Development</div>
+          </div>
+
         </div>
       </div>
 
