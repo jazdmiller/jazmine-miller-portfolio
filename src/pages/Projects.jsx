9a7b963282a91102578495594c552dd59bff149b
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-06 14:11:34 -0400

    projects page styling/layout

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,11 +3,11 @@ import SkillsIcon from '../../public/assets/images/star-icon.svg'
 
 function Projects() {
   return (
-    <div className='container-fluid text-center projects-container mx-5'>
+    <div className='container-fluid text-center projects-container mx-md-5'>
 
-      <div className='row'>
-        <div className='col-12 col-md-6 skills-col d-flex flex-column'>
-          <div className=' row mt-auto justify-content-start mr-0'>
+      <div className='row mr-0'>
+        <div className='col-12 col-md-6 skills-col d-flex flex-column pl-md-5'>
+          <div className=' row mt-md-auto justify-content-start mr-0'>
             <span className='skills-label'><img className='mr-3' src={SkillsIcon} />Skills</span>
           </div>
           <div className='row mr-0 skill-list '>
@@ -30,20 +30,20 @@ function Projects() {
           </div>
         </div>
 
-        <div className='col col-md-6 mr-auto project-col'>
-          <div>
+        <div className='col col-md-6 mr-auto project-col pl-5'>
+          <div className='mt-4'>
             <div className='project-title'>Shakn</div>
             <div className='project-subtext'>Design & Development</div>
           </div>
-          <div className='pt-4'>
+          <div className=' pt-4'>
             <div className='project-title'>Workit Landing Page</div>
-            <div className='project-subtext'>Development</div>
+            <div className='col project-subtext'>Development</div>
           </div>
           <div  className='pt-4'>
             <div className='project-title'>Shakn</div>
             <div className='project-subtext'>Design & Development</div>
           </div>
-          <div  className='pt-4'>
+          <div  className=' pt-4'>
             <div className='project-title'>Shakn</div>
             <div className='project-subtext'>Design & Development</div>
           </div>
