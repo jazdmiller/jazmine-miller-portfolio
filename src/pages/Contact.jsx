566875bf8185d0a4d3d1501e975532870c4cb46f
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-20 16:45:23 -0400

    add github link to projecs page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,39 +1,42 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import StarIcon from "/assets/images/star.svg";
-import { gsap } from 'gsap';
+import { gsap } from "gsap";
 
 function Contact() {
-
   useEffect(() => {
-    gsap.to('.contact-fade', {
+    gsap.to(".contact-fade", {
       opacity: 1,
       y: 0,
-      duration: .6,
-      stagger: .2,
-      ease: "power2.inOut"
-    })
-  })
+      duration: 0.6,
+      stagger: 0.2,
+      ease: "power2.inOut",
+    });
+  });
   return (
-    <div className='container contact-container'>
-      
-      <section className='contact-title'>
-        <div className='row'>
-          <h1 className='contact-fade'>Contact Me</h1>
-          <h2 className='contact-fade'>Feel free to reach out to discuss projects, collaborations, or just to say hello.</h2>
+    <div className="container contact-container">
+      <section className="contact-title">
+        <div className="row">
+          <h1 className="contact-fade">Contact Me</h1>
+          <h2 className="contact-fade">
+            Feel free to reach out to discuss projects, collaborations, or just
+            to say hello.
+          </h2>
         </div>
       </section>
 
-      <section className='email-row contact-fade'>
-        <div className='row'>
-         <p>Email: jazminedmiller@gmail.com</p>
+      <section className="email-row contact-fade">
+        <div className="row">
+          <p>Email: jazminedmiller@gmail.com</p>
         </div>
-        <div className='row'>
-        <p>Socials: <a>LinkedIn /</a><a> Github</a></p>
+        <div className="row">
+          <p>
+            Socials: <a>LinkedIn /</a>
+            <a> Github</a>
+          </p>
         </div>
       </section>
-
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,68 +1,71 @@
 import React, { useEffect, useRef } from "react";
 import SkillsIcon from "../../public/assets/images/star.svg";
 import DarkSkillsIcon from "../../public/assets/images/star-darkmode.svg";
-import { gsap } from 'gsap'
+import { gsap } from "gsap";
 
 function Projects({ isDarkMode }) {
-
-  const projectsRef = useRef(null)
+  const projectsRef = useRef(null);
 
   useEffect(() => {
     // const projects = projectsRef.current.children;
 
-    gsap.to('.project-list', {
+    gsap.to(".project-list", {
       opacity: 1,
       y: 0,
-      duration: .6,
-      stagger: .2,
+      duration: 0.6,
+      stagger: 0.2,
       ease: "power2.inOut",
-    })
+    });
 
-    gsap.to('.project-fade', {
+    gsap.to(".project-fade", {
       opacity: 1,
-      duration: .6,
-      ease: "power2.inOut"
-    })
-  }, [])
+      duration: 0.6,
+      ease: "power2.inOut",
+    });
+  }, []);
 
   return (
     <div className="container">
-    <div className="container-fluid text-center projects-container mx-md-5">
-      <div className="row mr-0">
-        <div className="col-12 col-md-6 skills-col d-flex flex-column project-fade ">
-          <div className=" row justify-content-start mr-0">
-            <span className="skills-label">
-              <img className="mr-1" src={isDarkMode ? DarkSkillsIcon : SkillsIcon} />
-              Projects
-            </span>
-          </div>
-          <div className="row mr-0 pl-2 skill-list ">
-            Showcase of my best projects utilizing technologies such as React,
-            Javascript, HTML/CSS, Bootstrap, Firebase, Nodejs, and Shopify. You can find more of my work on my github!
-          </div>
-         
-        </div>
+      <div className="container-fluid text-center projects-container mx-md-5">
+        <div className="row mr-0">
+          <div className="col-12 col-md-6 skills-col d-flex flex-column project-fade ">
+            <div className=" row justify-content-start mr-0">
+              <span className="skills-label">
+                <img
+                  className="mr-1"
+                  src={isDarkMode ? DarkSkillsIcon : SkillsIcon}
+                />
+                Projects
+              </span>
+            </div>
+            <div className="row mr-0 pl-2 skill-list ">
+              Showcase of some of my projects utilizing technologies such as
+              React, Javascript, HTML/CSS, Bootstrap, Firebase, Nodejs, and
+              Shopify. You can find more of my work on my <span className="m-0 git-link"><a>github</a></span>!
+            </div>
 
-        <div className="col col-md-6 mr-auto project-col" >
-          <div className="project-list">
-            <div className="project-title">Shakn</div>
-            <div className="project-subtext">Design & Development</div>
           </div>
-          <div className="project-list pt-4">
-            <div className="project-title">Workit Landing Page</div>
-            <div className="col project-subtext">Development</div>
-          </div>
-          <div className="project-list pt-4">
-            <div className="project-title">Shakn</div>
-            <div className="project-subtext">Design & Development</div>
-          </div>
-          <div className=" project-list pt-4">
-            <div className="project-title">Shakn</div>
-            <div className="project-subtext">Design & Development</div>
+
+          <div className="col col-md-6 mr-auto project-col">
+            <div className="project-list">
+              <div className="project-title">Shakn</div>
+              <div className="project-subtext">Design & Development</div>
+            </div>
+            <div className="project-list pt-4">
+              <div className="project-title">Workit Landing Page</div>
+              <div className="col project-subtext">Development</div>
+            </div>
+            <div className="project-list pt-4">
+              <div className="project-title">Shakn</div>
+              <div className="project-subtext">Design & Development</div>
+            </div>
+            <div className=" project-list pt-4">
+              <div className="project-title">Shakn</div>
+              <div className="project-subtext">Design & Development</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
