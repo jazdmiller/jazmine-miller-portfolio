a396c165e88f7b7afa3af3c31a8fe70527608d46
Jazmine Miller <jazminedmiller@gmail.com>
2023-10-23 14:58:51 -0400

    add firebase cofig/imports to projects page

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCVMOfdmNt6IeWmoDJdK-zwE37dJ9gkpoA",
+  authDomain: "portfolio-jazmine-miller.firebaseapp.com",
+  projectId: "portfolio-jazmine-miller",
+  storageBucket: "portfolio-jazmine-miller.appspot.com",
+  messagingSenderId:"360880565536",
+  appId: "1:360880565536:web:3fd5130a66d4b31396bb45"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app)
+
+export { firestore }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,31 +1,56 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import SkillsIcon from "../../public/assets/images/star.svg";
 import DarkSkillsIcon from "../../public/assets/images/star-darkmode.svg";
 import { gsap } from "gsap";
+import { firestore } from '../firebase'
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
+import { Link } from "react-router-dom";
 
 function Projects({ isDarkMode }) {
-  const projectsRef = useRef(null);
+  
+  const [projects, setProjects] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
-    // const projects = projectsRef.current.children;
-
-    gsap.to(".project-list", {
-      opacity: 1,
-      y: 0,
-      duration: 0.6,
-      stagger: 0.2,
-      ease: "power2.inOut",
-    });
-
-    gsap.to(".project-fade", {
-      opacity: 1,
-      duration: 0.6,
-      ease: "power2.inOut",
-    });
+    const fetchProjects = async () => {
+      const projectsCollection = collection(firestore, "projects")
+      const projectsQuery = query(projectsCollection, orderBy("order", "asc"))
+      const snapshot = await getDocs(projectsQuery)
+      const projectData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      console.log(projectData)
+      setProjects(projectData)
+      setIsLoading(false)
+    
+  }
+  fetchProjects()
   }, []);
+
+  useEffect(() => {
+    if(!isLoading) {
+      gsap.to(".project-list", {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        stagger: 0.2,
+        ease: "power2.inOut",
+      });
+  
+      gsap.to(".project-fade", {
+        opacity: 1,
+        duration: 0.6,
+        ease: "power2.inOut",
+      });
+    }
+  }, [isLoading])
 
   return (
     <div className="container">
+      {isLoading && <div>Loading...</div>}
+      {!isLoading && (
+
       <div className="container-fluid text-center projects-container mx-md-5">
         <div className="row mr-0">
           <div className="col-12 col-md-6 skills-col d-flex flex-column project-fade ">
@@ -47,10 +72,17 @@ function Projects({ isDarkMode }) {
           </div>
 
           <div className="col col-md-6 mr-auto project-col">
-            <div className="project-list">
-              <div className="project-title">Shakn</div>
-              <div className="project-subtext">Design & Development</div>
-            </div>
+            {projects.map((project) => (
+              <div className="project-list pt-4">
+
+              <Link key={project.id} to={`/projects/${project.id}`} >
+                
+                  <div className="project-title">{project.title}</div>
+                  <div className="project-subtext">{project.meta_description}</div>
+                
+              </Link>
+              </div>
+            ))}
             <div className="project-list pt-4">
               <div className="project-title">Workit Landing Page</div>
               <div className="col project-subtext">Development</div>
@@ -66,6 +98,7 @@ function Projects({ isDarkMode }) {
           </div>
         </div>
       </div>
+      )}
     </div>
   );
 }
